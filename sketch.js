2b890c47c38c96ec109772142b4f1c6302c33718
neelm1010 <69491453+neelm1010@users.noreply.github.com>
2020-12-28 16:26:05 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(800,400);
+  var hr,mn,sc;
+  hr = hour();
+  mn = minute();
+  sc = seconds();
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(255,255,255);  
+  scAngle = map(sc,0,60,0,360)
+  hrAngle = map(hr,0,12,0,360)
+  mnAngle = map(mn,0,60,0,360)
+  stroke(255,0,0)
+  strokeWeight(7);
+  line(0,0,100,0);
+  drawSprites();
+}
